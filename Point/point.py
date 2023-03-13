e5a5aa97bcced8d102c126923c145df0c5592fbb
skadakar <dale.ole.morten@gmail.com>
2023-03-13 23:18:41 +0100

    fuckity!

--- Point/point.py
+++ Point/point.py
@@ -11,7 +11,6 @@
 import sys
 import json
 import time
-
 from gpiozero import Button
 from gpiozero import Buzzer
 
@@ -32,26 +31,38 @@ yellow_button = Button(27, bounce_time=0.2)
 # Setup all default values:
 name = ""
 mode = ""
+start_color = ""
 capture_time = 30
-startcolor = ""
-hasbuzzer = 0
+has_buzzer = 0
+spawn_buzzer = 0
+spawn_main_cycle = 30
+spawn_rally_cycle = 15
 start_time = 0
+spawn_cycle_timer = 0
+spawn_cycle_timer_start = time.time()
 
 # load the configuration data from the JSON file
 with open("config.json", "r") as f:
     config_data = json.load(f)
+    print("Loaded config:")
+    print(config_data)
 
 # Get keys from config
 for key, value in config_data.items():
     globals()[key] = value
 
+
 # connect to meshtastic
-def sendmessage(poop):
+import meshtastic
+import meshtastic.serial_interface
+
+def send_message(poop):
     iface = meshtastic.serial_interface.SerialInterface()
     iface.sendText(poop)
     iface.close()
 
-def buzzercapcomplete():
+
+def buzzer_cap_complete():
     print(f"Playing capture sound")
     for i in range(5):
         bz.on()
@@ -60,19 +71,27 @@ def buzzercapcomplete():
         bz.off()
         time.sleep(0.25)
 
+def buzzer_spawn_cycle():
+    global spawn_cycle_timer_start
+    print(f"Time for a {mode} respawn!")
+    for i in range(3):
+        bz.on()
+        print(f"BEEP")
+        time.sleep(1)
+        bz.off()
+        time.sleep(0.25)
+    print(f"Resetting spawn cycle!")
+    spawn_cycle_timer_start = time.time()
+    return spawn_cycle_timer_start
+
 # send initial state
 def send_initial_state():
     print(f"Sending initial state of the point")
     # create a message to send in meshtastic
-    sendmessage(f"/mesh/points/{name}/{mode}/status/{startcolor}")
+    send_message(f"/mesh/points/{name}/{mode}/status/{start_color}")
     print(f"Initial message sent")
 
 #Sending initial state from config.json
-print(f"Point: {name}")
-print(f"Mode: {mode}")
-print(f"Starts as: {startcolor}")
-print(f"Capture time: {capture_time}")
-print(f"Buzzer: {hasbuzzer}")
 send_initial_state()
 
 def button_pressed(color):
@@ -83,16 +102,16 @@ def button_pressed(color):
     # check if the button has been held for 30 seconds
     if time.time() - start_time >= capture_time:
         print(f"Point {name} has been captured by {color}!")
-        sendmessage(f"/mesh/points/{name}/{mode}/status/{color}")
+        send_message(f"/mesh/points/{name}/{mode}/status/{color}")
         # play capture buzz if enabled
-        if hasbuzzer == 1:
-            buzzercapcomplete()
+        if has_buzzer == 1:
+            buzzer_cap_complete()
         time.sleep(5)
         # reset the start time
         start_time = 0
     else:
-        timeheld = int(time.time() - start_time)
-        print(f"Capture started for {color}. Held for {timeheld}seconds.")
+        time_held = int(time.time() - start_time)
+        print(f"Capture started for {color}. Held for {time_held}seconds.")
         time.sleep(2)
     return start_time
 
@@ -104,13 +123,20 @@ while True:
     if red_button.is_pressed:
         start_time = button_pressed("red")
     # check if blue_button is held
-    elif blue_button.is_pressed:
+    if blue_button.is_pressed:
         start_time = button_pressed("blue")
     # check if yellow_button is held
-    elif yellow_button.is_pressed:
+    if yellow_button.is_pressed:
         start_time = button_pressed("yellow")
-    # no button is pressed, reset start time
-    else:
+    #Check spawn cycle buzzer is enabled
+    if spawn_buzzer == 1:
+        # Check if it's time to respawn.
+        if mode == "spawn" and time.time() - spawn_cycle_timer_start >= spawn_main_cycle * 60:
+            buzzer_spawn_cycle()
+        if mode == "rally" and time.time() - spawn_cycle_timer_start >= spawn_rally_cycle * 60:
+            buzzer_spawn_cycle()
+    # no button is pressed and it's not time to spawn.
+    elif:
         start_time = 0
     # sleep for a short amount of time to avoid consuming too much CPU
-    time.sleep(0.1)
+    time.sleep(0.5)
