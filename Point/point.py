2befb373332b1b649fd88fc85a7622034092b526
skadakar <dale.ole.morten@gmail.com>
2023-03-14 23:13:15 +0100

    Make pretty

--- Point/point.py
+++ Point/point.py
@@ -8,28 +8,19 @@
 #
 # There is no differentiation based on mode currently, as this is expected to be for expansion or mostly handled by the gateway.
 
-import sys
 import json
 import time
-from gpiozero import Button
-from gpiozero import Buzzer
 
 import meshtastic
 import meshtastic.serial_interface
+from gpiozero import Button
+from gpiozero import Buzzer
 
 # set up GPIO pins for buttons
-# 17 being red
-# 22 being blue
-# 27 being yellow (or neutral)
-# 23 being buzzer
-
 bz = Buzzer(23)
 red_button = Button(17, bounce_time=0.05)
 blue_button = Button(22, bounce_time=0.05)
 yellow_button = Button(27, bounce_time=0.05)
-
-
-
 
 # Setup all default values:
 name = ""
@@ -56,6 +47,7 @@ with open("config.json", "r") as f:
 for key, value in config_data.items():
     globals()[key] = value
 
+
 def send_message(text):
     iface = meshtastic.serial_interface.SerialInterface()
     iface.sendText(text)
@@ -72,6 +64,7 @@ def buzzer_cap_complete():
             bz.off()
             time.sleep(0.25)
 
+
 def buzzer_spawn_cycle():
     global spawn_cycle_timer_start
     print(f"Time for a {mode} respawn!")
@@ -85,6 +78,7 @@ def buzzer_spawn_cycle():
     spawn_cycle_timer_start = time.time()
     return spawn_cycle_timer_start
 
+
 # send initial state
 def send_initial_state():
     print(f"Sending initial state of the point")
@@ -95,8 +89,10 @@ def send_initial_state():
         send_message(f"/mesh/points/{name}/{mode}/status/{start_color}")
     print(f"Initial message sent")
 
-#Sending initial state from config.json
+
+# Sending initial state from config.json
 send_initial_state()
+
 
 def capture_complete(color):
     print(f"Point {name} has been captured by {color}!")
@@ -109,7 +105,8 @@ def capture_complete(color):
     print(f"Returning to looplife")
     time.sleep(5)
 
-def capture_time_check(button,color):
+
+def capture_time_check(button, color):
     if button.is_held == True:
         if button.held_time < float(1):
             print(f"Capture attempt started by {color}.Capture time is {capture_time} seconds")
@@ -118,6 +115,8 @@ def capture_time_check(button,color):
         if button.held_time > capture_time:
             print(f"Capture completed by {color}, held for {button.held_time}.")
             capture_complete(color)
+
+
 def spawn_refresh():
     print(f"Refreshing {start_color} spawn {name}")
     send_message(f"/mesh/points/{name}/{mode}/status/{color}")
@@ -129,11 +128,11 @@ def spawn_refresh():
 print(f"Starting looping forever")
 
 while True:
-    #Spam spawn status at frequent intervals
+    # Spam spawn status at frequent intervals
     if time.time() > spawn_last_spam + spawn_refresh_cycle * 60:
         spawn_refresh()
 
-    #Check for button presses:
+    # Check for button presses:
     if blue_button.is_active:
         capture_time_check(blue_button, "blue")
     if red_button.is_active:
@@ -141,14 +140,13 @@ while True:
     if yellow_button.is_active:
         capture_time_check(yellow_button, "yellow")
 
-    #Check buzzer and spawn cycle buzzer is enabled
+    # Check buzzer and spawn cycle buzzer is enabled
     if spawn_buzzer == 1 and has_buzzer == 1:
         # Check if it's time to respawn.
         if mode == "spawn" and time.time() - spawn_cycle_timer_start >= spawn_main_cycle * 60:
             buzzer_spawn_cycle()
         if mode == "rally" and time.time() - spawn_cycle_timer_start >= spawn_rally_cycle * 60:
             buzzer_spawn_cycle()
-
 
     # sleep for a short amount of time to avoid consuming too much CPU
     time.sleep(0.1)
